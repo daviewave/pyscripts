193ab9e2fda37e7f9104500317acd3c8b558bc02
ds <dav.silveira@proton.me>
2025-03-17 16:31:20 -0400

    spent a lonnnnnnnnnng time making the common_tools class to make this code look so simple and organzied and save time moving forward

--- pegasus/idevicebackup/backup_iphone_data.py
+++ pegasus/idevicebackup/backup_iphone_data.py
@@ -1,29 +1,108 @@
-import argparse, os
+import argparse, sys, os
+
+script_dir = os.path.abspath(__file__)
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.environ.get("py")))
+)
+
+from common_tools import EnvUtils, IOUtils
 
 
-def backup_iphone_data(args):
+# - 1.
+def check_deps():
+    # a)
+    env = EnvUtils()
+    brew_requirements_fp = f"{script_dir}/brew-requirements.txt"
+    pip_requirements_fp = f"{script_dir}/brew-requirements.txt"
+
+    print(f"brfp: {brew_requirements_fp}")
+    print(f"prfp: {pip_requirements_fp}")
+
+    # b)
+    has_brew_dependencies = env.has_brew_deps(brew_requirements_fp)
+
+    # c)
+    has_pip_dependencies = env.has_brew_deps(pip_requirements_fp)
+
+    return has_brew_dependencies and has_pip_dependencies
+
+
+# - 2.
+def get_target_iphone_id():
     pass
+
+
+# - 3.
+def pair_iphone(iphone_id):
+    pass
+
+
+# - 4.
+def enable_encryption():
+    pass
+
+
+# - 5.
+def run_backup():
+    pass
+
+
+# == main
+def backup_iphone_data(no_encrypt, full_backup, output_dir):
+    io = IOUtils()
+
+    # 1.
+    io.start_msg(
+        status="1/5",
+        func="check_deps()",
+        message="checking your system for required depencencies...",
+    )
+    check_deps()
+    io.done(newline=True)
+
+    # 2.
+
+    # 3.
+
+    # 4.
+
+    # 5.
+
+    io.success_msg(f"iphone data backed up at: {output_dir}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
 
     parser.add_argument(
-        "--decrypt-dir",
-        type=str,
+        "--dont-encrypt",
+        action="store_true",
         help="location of the directory containing the idevicebackup2 decrypted backup data",
     )
 
     parser.add_argument(
+        "-f",
+        "--full",
+        action="store_true",
+        help="location of the directory containing the idevicebackup2 decrypted backup data",
+    )
+
+    parser.add_argument(
+        "-o",
         "--output",
-        required=False,
         default="",
+        required=False,
+        # required=True,
         help="if you want to store the output to a file include this flag followed by the fp to store at",
     )
 
+    # NOTE: could allow the deps files as optional arguments
     args = parser.parse_args()
+    no_encrypt = args.dont_encrypt
+    full_backup = args.full
+    output_dir = args.output
 
-    backup_iphone_data(args)
+    backup_iphone_data(no_encrypt, full_backup, output_dir)
 
 
 # pseudo-code
@@ -36,7 +115,7 @@ if __name__ == "__main__":
 #       --> check brew packages
 #       --> check python/pip packages
 
-# 2. get_connected_iphone_id()
+# 2. get_target_iphone_id()
 #       --> run `idevice_id -l` to list iphones connected to computer and:
 #       --> if 0 returned, return error message
 #       --> if 1 returned, return id
@@ -55,7 +134,7 @@ if __name__ == "__main__":
 #           (*)--> if exception hits in either make sure error does say its already enabled,
 #                   print info_msg (compare_str="Backup encryption is already enabled")
 
-# 5. backup_iphone_data(iphone_id, output_dir, encrypt_data, full, password)
+# 5. run_backup(iphone_id, output_dir, encrypt_data, full, password)
 #       --> run backup command (adding --full if necessary), save to output dir
 
 # 6. (maybe) make_backup_dir_immutable(immutable_arg)
