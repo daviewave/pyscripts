46c5d49067e8d07a68401879709e1447362dd5f5
ds <dav.silveira@proton.me>
2025-03-19 03:59:48 -0400

    couldnt help myself had to clean code before sleep lmafao

--- debug-bins/scripts/extract_c_code.py
+++ debug-bins/scripts/extract_c_code.py
@@ -8,32 +8,47 @@ sys.path.append(
 
 from common_tools import IOUtils, FsUtils, FileUtils
 
+io = IOUtils()
 
 
+def get_c_code(fp, func):
+    try:
+        r2 = r2pipe.open(fp)
+        r2.cmd("aaaa")
+        r2.cmd(f"s {func}")
+        c_code = r2.cmd("pdg")
+        return c_code
+    except Exception as e:
+        io.error_msg(status="1/2", func="get_c_code()")
+        raise Exception(e)
 
-def extract_c_code(fp, func, json_format):
-    fs = FsUtils()
-    io = IOUtils()
-    fs.is_valid_path(fp, strict="file")
-
-    # a) 
-    r2 = r2pipe.open(fp)
-    r2.cmd("aaaa")
-    r2.cmd(f"s {func}")
-    c_code = r2.cmd("pdg")
-
-    # b) 
+def prep_output_dir(fp):
     f = FileUtils()
+    fs = FsUtils()
     bin_file = os.path.basename(fp)
     results_dir = f"{script_dir}/../reuslts/{bin_file}"
     results_dir = fs.ensure_absolute_path(results_dir)
     if not fs.exists(results_dir):
         fs.create_dir(results_dir)
     results_file = f"{results_dir}/{bin_file}-main-c.c"
+    return results_file
 
-    f.write(results_file, c_code)
+def extract_c_code(fp, func, json_format):
+    # a) 
+    fs = FsUtils()
+    fs.is_valid_path(fp, strict="file")
 
-    io.success_msg(f"deassembled '{bin_file}' into C code at: {results_file}")
+    # b) 
+    c_code = get_c_code(fp, func)
+
+    # c) 
+    output_fp = prep_output_dir(fp)
+
+    # d)
+    f = FileUtils()
+    f.write(output_fp, c_code)
+
+    io.success_msg(f"deassembled '{os.path.basename(fp)}' into C code at: {output_fp}")
 
 
 if __name__ == "__main__":
